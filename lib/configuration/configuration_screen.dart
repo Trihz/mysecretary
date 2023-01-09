66f802acaadfa33752b891269898e0c1522599c3
Trihz <stephenmusangi8264@gmail.com>
2023-01-09 15:13:19 +0300

    Configuration Screen

---
+++ lib/configuration/configuration_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ConfigurationScreen extends StatefulWidget {
+  const ConfigurationScreen({super.key});
+
+  @override
+  State<ConfigurationScreen> createState() => _ConfigurationScreenState();
+}
+
+class _ConfigurationScreenState extends State<ConfigurationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Container(
+      height: MediaQuery.of(context).size.he
+    ));
+  }
+}
